7621638dbcb13a35027039980d73a62f4eb8cb1d
NotBuer <rodrigosoocer@gmail.com>
2025-09-13 20:41:56 -0300

    refactor: boilerplate shader error check code removed

--- EngineCore/include/EngineCore/Graphics/Shader.hpp
+++ EngineCore/include/EngineCore/Graphics/Shader.hpp
@@ -1,8 +1,21 @@
 #pragma once
 #include <string>
+#include <ostream>
 
 namespace engine::graphics
 {
+
+	enum class ShaderType : uint8_t { VERTEX, FRAGMENT };
+
+	inline std::ostream& operator<<(std::ostream& os, ShaderType t)
+	{
+		switch (t)
+		{
+		case ShaderType::VERTEX: return os << "VERTEX";
+		case ShaderType::FRAGMENT: return os << "FRAGMENT";
+		}
+		return os << "UNKNOW";
+	}
 
 	class Shader
 	{
@@ -15,6 +28,9 @@ namespace engine::graphics
 		void setBool(const std::string& name, bool value) const;
 		void setInt(const std::string& name, int value) const;
 		void setFloat(const std::string& name, float value) const;
+
+		void checkShaderCompilationErrors(unsigned int id, ShaderType type, int& success, char* infoLog);
+		void checkShaderProgramLinkingErrors(unsigned int id, int& success, char* infolog);
 
 	private:
 		unsigned int Id;
--- EngineCore/src/Graphics/Shader.cpp
+++ EngineCore/src/Graphics/Shader.cpp
@@ -5,7 +5,7 @@
 
 namespace engine::graphics
 {
-	// TODO: refactor boilerplate code to common function later on...
+
 	Shader::Shader(const char* vPath, const char* fPath)
 	{
 		std::string vertexCode;
@@ -35,39 +35,24 @@ namespace engine::graphics
 		const char* fShaderCode = fragmentCode.c_str();
 
 		unsigned int vertex, fragment;
-		int success;
-		char infoLog[512];
+		int success = 0;
+		char infoLog[512] = {};
 
 		vertex = glCreateShader(GL_VERTEX_SHADER);
 		glShaderSource(vertex, 1, &vShaderCode, NULL);
 		glCompileShader(vertex);
-		glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-		if (!success)
-		{
-			glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
+		checkShaderCompilationErrors(vertex, engine::graphics::ShaderType::VERTEX, success, infoLog);
 
 		fragment = glCreateShader(GL_FRAGMENT_SHADER);
 		glShaderSource(fragment, 1, &fShaderCode, NULL);
 		glCompileShader(fragment);
-		glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-		if (!success)
-		{
-			glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
+		checkShaderCompilationErrors(vertex, engine::graphics::ShaderType::FRAGMENT, success, infoLog);
 
 		Id = glCreateProgram();
 		glAttachShader(Id, vertex);
 		glAttachShader(Id, fragment);
 		glLinkProgram(Id);
-		glGetProgramiv(Id, GL_LINK_STATUS, &success);
-		if (!success)
-		{
-			glGetProgramInfoLog(Id, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-		}
+		checkShaderProgramLinkingErrors(Id, success, infoLog);
 
 		glDeleteShader(vertex);
 		glDeleteShader(fragment);
@@ -92,7 +77,27 @@ namespace engine::graphics
 
 	void Shader::setFloat(const std::string& name, float value) const
 	{
-		glUniform1f(glGetUniformLocation(Id, name.c_str()), (int)value);
+		glUniform1f(glGetUniformLocation(Id, name.c_str()), value);
+	}
+
+	void Shader::checkShaderCompilationErrors(unsigned int id, ShaderType type, int &success, char *infoLog)
+	{
+		glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(id, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::" << type  << "::COMPILATION_FAILED" << infoLog << std::endl;
+		}
+	}
+
+	void Shader::checkShaderProgramLinkingErrors(unsigned int id, int &success, char *infoLog)
+	{
+		glGetProgramiv(id, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(id, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
 	}
 
 }
